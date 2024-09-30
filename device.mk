ddfa01d9a3ac272fda3257c3a4c82b021f816009
chongkessob <chonks091004@gmail.com>
2024-09-30 17:29:19 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,3 +1,28 @@
-# Enable project quotas and casefolding for emulated storage without sdcardfs - 
-# SDCard replacement functionality
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+LOCAL_PATH := device/tecno/TECNO-LG8n
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
+
+PRODUCT_PACKAGES += \
+    bootctrl.mt6789
+
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.mt6789 \
+    libgptutils \
+    libz \
+    libcutils
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload

7fa9cb3f3e8a542451f27fcefc174e5237b14649
chongkessob <chonks091004@gmail.com>
2024-10-01 16:29:16 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-     $(LOCAL_DIR)/twrp_TECNO-LG8n.mk
+     $(LOCAL_DIR)/twrp_LG8n.mk
 
 COMMON_LUNCH_CHOICES := \
-     twrp_TECNO-LG8n-user \
-     twrp_TECNO-LG8n-userdebug \
-     twrp_TECNO-LG8n-eng
+     twrp_LG8n-user \
+     twrp_LG8n-userdebug \
+     twrp_LG8n-eng

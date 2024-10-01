567ca404a27f60e047af067b132f0b8dd0641d32
chongkessob <chonks091004@gmail.com>
2024-10-01 17:21:17 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_TECNO-LG8n-user
-add_lunch_combo omni_TECNO-LG8n-userdebug
-add_lunch_combo omni_TECNO-LG8n-eng
+add_lunch_combo twrp_LG8n-user
+add_lunch_combo twrp_LG8n-userdebug
+add_lunch_combo twrp_LG8n-eng
